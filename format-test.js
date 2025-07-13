cdcc6c261a3341b7c5c57c6cf64c4fae57de4a9c
zandy2test <511uefn@gmail.com>
2025-07-13 05:44:14 +0200

    Add format-test.js - Testing code formatting capabilities

---
+++ format-test.js
@@ -0,0 +1,9 @@
+// This file tests auto-formatting functionality
+const   messyCode={name:"test",value:123,items:[1,2,3,4,5]};
+function   badlyFormatted(  param1,param2  ){
+return{
+result:param1+param2,
+status:"success"
+}
+}
+const arrow=(x,y)=>{return x*y}
